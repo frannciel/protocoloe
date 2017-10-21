fb1324f5f7f832497e8aff8deeca32514e9a128b
Franciel <frannciel@gmail.com>
2017-10-20 22:27:01 -0300

    Update email.php

--- views/email.php
+++ views/email.php
@@ -1,7 +1,7 @@
 <?php
 
 # Include the Autoloader (see "Libraries" for install instructions)
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 use Mailgun\Mailgun;
 
 # Instantiate the client.
