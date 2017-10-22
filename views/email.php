ffc3c5e25f07ca1d935e31b49d8293352ae6a04d
Franciel <frannciel@gmail.com>
2017-10-22 03:19:06 -0300

    Update email.php

--- views/email.php
+++ views/email.php
@@ -1,6 +1,13 @@
 # Using Awesome https://github.com/PHPMailer/PHPMailer
 <?php
 print_r( parse_url(getenv("MAILGUN_API_KEY")));
+print_r( parse_url(getenv("MAILGUN_DOMAIN")));
+print_r( parse_url(getenv("MAILGUN_PUBLIC_KEY")));
+print_r( parse_url(getenv("MAILGUN_SMTP_LOGIN")));
+print_r( parse_url(getenv("MAILGUN_SMTP_PASSWORD")));
+print_r( parse_url(getenv("MAILGUN_SMTP_PORT")));
+print_r( parse_url(getenv("MAILGUN_SMTP_SERVER")));
+
 echo 'MAILGUN_API_KEY'.'</br>';
 echo 'MAILGUN_DOMAIN'.'</br>';
 echo 'MAILGUN_PUBLIC_KEY'.'</br>';
