5ce287a6831dbc06c4432c2e449fa01099f5ffe6
frannciel <frannciel@yahoo.com.br>
2017-10-22 02:47:10 -0300

    Revert "adicionando PHPMiler"
    This reverts commit c102765320ca72d875684f528e013c8d0ebeadef.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -23,7 +23,6 @@ class ComposerStaticInitc0136a3610f886d22ebb291d2ef60d80
         'P' => 
         array (
             'Psr\\Http\\Message\\' => 17,
-            'PHPMailer\\PHPMailer\\' => 20,
         ),
         'H' => 
         array (
@@ -52,10 +51,6 @@ class ComposerStaticInitc0136a3610f886d22ebb291d2ef60d80
         'Psr\\Http\\Message\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/http-message/src',
-        ),
-        'PHPMailer\\PHPMailer\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Http\\Promise\\' => 
         array (
