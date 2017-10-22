f60e8c0fe0d3d7888145190f3b32d609b229b571
Franciel <frannciel@gmail.com>
2017-10-22 03:11:15 -0300

    Update email.php

--- views/email.php
+++ views/email.php
@@ -1,31 +1,10 @@
-// Using Awesome https://github.com/PHPMailer/PHPMailer
+# Using Awesome https://github.com/PHPMailer/PHPMailer
 <?php
-require '../vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
-
-$mail = new PHPMailer;
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mailgun.org';                     // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'MAILGUN_SMTP_LOGIN';              // SMTP username
-$mail->Password = 'MAILGUN_SMTP_PASSWORD';             // SMTP password
-$mail->Port     = 'MAILGUN_SMTP_PORT';
-$mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
-
-$mail->From = 'frannciel@gmail.com';
-$mail->FromName = 'Mailer';
-$mail->addAddress('frannciel.edu@gmail.com');                 // Add a recipient
-
-$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-
-$mail->Subject = 'Hello';
-$mail->Body    = 'Testing some Mailgun awesomness';
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
-
+echo 'MAILGUN_API_KEY'.'</br>';
+echo 'MAILGUN_DOMAIN'.'</br>';
+echo 'MAILGUN_PUBLIC_KEY'.'</br>';
+echo 'MAILGUN_SMTP_LOGIN'.'</br>';
+echo 'MAILGUN_SMTP_PASSWORD'.'</br>';
+echo 'MAILGUN_SMTP_PORT'.'</br>';
+echo 'MAILGUN_SMTP_SERVER'.'</br>';
 ?>
