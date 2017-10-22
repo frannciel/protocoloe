7127bffde2f5335c8e9fe43e8be54f9f7fd0d57f
Franciel <frannciel@gmail.com>
2017-10-22 19:20:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,21 +8,23 @@ require 'vendor/autoload.php';
 //$mail = new PHPMailer();
 $mail = new PHPMailer;
 
+$mail->SMTPDebug = 2;
+
 $mail->isSMTP();                                        // Set mailer to use SMTP
-$mail->Host = 'smtp.mailgun.org';                       // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                                 // Enable SMTP authentication
-$mail->Username = getenv("MAILGUN_SMTP_LOGIN");         // SMTP username
-$mail->Password = getenv("MAILGUN_SMTP_PASSWORD");      // SMTP password
-$Mailer->Port   = getenv("MAILGUN_SMTP_PORT");
+$mail->Username = getenv("EMAIL_SMTP_LOGIN");         // SMTP username
+$mail->Password = getenv("EMAIL_SMTP_PASSWORD");      // SMTP password
+$Mailer->Port   = getenv("EMAIL_SMTP_PORT");
 $mail->SMTPSecure = 'tls';                              // Enable encryption, only 'tls' is accepted
 
-$mail->From = getenv("MAILGUN_SMTP_LOGIN");
+$mail->From = getenv("EMAIL_SMTP_LOGIN");
 $mail->FromName = 'Mailer';
 $mail->addAddress('frannciel.edu@gmail.com');                 // Add a recipient
 
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 
-$mail->Subject = 'Hello';
+$mail->Subject = 'Ola testando ';
 $mail->Body    = 'Testing some Mailgun awesomness';
 
 if(!$mail->send()) {
